bf1584079b439d7452709484777314d4ef1e7f0a
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-05-22 11:42:06 +0100

    0x04 - fix #7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,7 +18,7 @@ def states_list():
     UL tag: with the list of all State objects present in DBStorage sorted
     by name (A->Z)
     """
-    states = sorted(list(storage.all("States").values()), key=lambda x: x.name)
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
 
