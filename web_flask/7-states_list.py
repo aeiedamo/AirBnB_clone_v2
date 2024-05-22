9e76bd6c0d01638f88a36e2a22f84682952a3fe9
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-05-22 11:30:54 +0100

    0x04 - add #7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+import models
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+        display a HTML page:
+        H1 tag: “States”
+    UL tag: with the list of all State objects present in DBStorage sorted
+    by name (A->Z)
+    """
+    states = sorted(list(models.storage.all("States").values()), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exeception):
+    """closes the storage on teardown"""
+    models.storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
